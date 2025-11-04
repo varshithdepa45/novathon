bc0e4466bf8d214564f52fa5622f11606f94e2e5
varshithdepa45 <varshithreddy987@gmail.com>
2025-11-04 13:07:16 +0530

    2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,16 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5174,
-    host: true,
-    strictPort: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
 })
